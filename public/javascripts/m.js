ca06ec56b89cbf2e0037baadf475b6fd82c0c8a1
Jeff.D <jeff.d@live.cn>
2016-08-12 15:37:53 +0800

    bug fix

--- public/javascripts/m.js
+++ public/javascripts/m.js
@@ -208,7 +208,7 @@
 			_closeList();
 		});
 
-		$('input[type=number]').on('keyup touchend', function(){
+		$('#cresult').on('change', function(){
 			$('[id^="amt_"').each(function(){
 				var code = $(this).attr('id').split('_')[1];
 				_fetchRate(code, 0);
