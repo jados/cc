799d50619234cfadfcdd0d574cf1358a2623ab73
Jeff.D <jeff.d@live.cn>
2016-08-12 16:01:09 +0800

    bug fix

--- public/javascripts/m.js
+++ public/javascripts/m.js
@@ -208,7 +208,7 @@
 			_closeList();
 		});
 
-		$('#cresult').keypress(function(){
+		$('#cresult').keydown(function(){
 			$('[id^="amt_"').each(function(){
 				var code = $(this).attr('id').split('_')[1];
 				_fetchRate(code, 0);
